379afd5fb19927405403197b35bd2fdc7cbbdf4e
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-26 20:43:04 +0800

    save to global variable

--- Degrees/Degrees/main.c
+++ Degrees/Degrees/main.c
@@ -9,10 +9,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Declare a global variable
+float lastTemperature;
+
 float fahrenheitFromCelsius(float cel)
 {
+    lastTemperature = cel;
     float fahr = cel * 1.8 + 32.0;
     printf("%f Celsius is %f Fahrenheit\n", cel, fahr);
+    printf("The last temperature converted was %f\n", lastTemperature);
     return fahr;
 }
 
