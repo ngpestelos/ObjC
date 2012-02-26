5103f506d3f7aa95a5c5c645e8cffab51341864c
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-26 12:36:54 +0800

    add two floating-point numbers

---
+++ TwoFloats/TwoFloats/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Nestor Pestelos on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    float alpha = 3.14;
+    float beta = 42.0;
+    double sum = alpha + beta;
+    
+    printf("Sum is %f", sum); // 45.139999
+
+    return 0;
+}
+
