a06695087325a10a0c61990be79da1562b52c78f
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-26 12:59:50 +0800

    intro to functions

---
+++ ClassCertificates/ClassCertificates/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  ClassCertificates
+//
+//  Created by Nestor Pestelos on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    printf("Mark has done as much Cocoa Programming as I could fit into 5 days\n");
+    
+    printf("Bo has done as much Objective-C Programming as I could fit into 2 days\n");
+    
+    printf("Ted has done as much iOS Programming as I could fit into 5 days\n");
+    
+    return 0;
+}
+
