bf0b2f22d63094b706da4ca3d819abaffd7b8817
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-26 20:46:55 +0800

    print an integer in base-10, base-8, and base-16

---
+++ Numbers/Numbers/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Nestor Pestelos on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    int x = 255;
+    printf("x is %d.\n", x);
+    printf("In octal, x is %o.\n", x);
+    printf("In hexadecimal, x is %x.\n", x);
+    return 0;
+}
+
