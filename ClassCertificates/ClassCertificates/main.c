52ce07d878ebb9de56d2f744897bd3b0d4ca481a
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-26 13:40:52 +0800

    added sleep

--- ClassCertificates/ClassCertificates/main.c
+++ ClassCertificates/ClassCertificates/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void congratulateStudent(char *student, char *course, int numDays) {
     printf("%s has done as much %s Programming as I could fit into %d days.\n", student, course, numDays);
@@ -22,8 +23,11 @@ int main (int argc, const char * argv[])
     //printf("Ted has done as much iOS Programming as I could fit into 5 days\n");
     
     congratulateStudent("Mark", "Cocoa", 5);
+    sleep(2);
     congratulateStudent("Bo", "Objective-C", 2);
+    sleep(2);
     congratulateStudent("Ted", "iOS", 5);
+    sleep(2);
     
     return 0;
 }
