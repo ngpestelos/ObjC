f0b332c02adce0260eb568fc5e61bcbd7aaaec56
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-26 12:26:51 +0800

    a good start

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Nestor Pestelos on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worse of times.\n");
+    
+    /* Is that actually any good?
+       Maybe it needs a rewrite. */
+    
+    return 0;
+}
+
