142f4f20919af2861688de2ae6e270f4b17289b5
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-26 13:25:24 +0800

    call a function; ignore warnings

--- ClassCertificates/ClassCertificates/main.c
+++ ClassCertificates/ClassCertificates/main.c
@@ -8,14 +8,22 @@
 
 #include <stdio.h>
 
+void congratulateStudent(char *student, char *course, int numDays) {
+    printf("%s has done as much %s Programming as I could fit into %d days.\n", student, course, numDays);
+}
+
 int main (int argc, const char * argv[])
 {
 
-    printf("Mark has done as much Cocoa Programming as I could fit into 5 days\n");
+    //printf("Mark has done as much Cocoa Programming as I could fit into 5 days\n");
     
-    printf("Bo has done as much Objective-C Programming as I could fit into 2 days\n");
+    //printf("Bo has done as much Objective-C Programming as I could fit into 2 days\n");
     
-    printf("Ted has done as much iOS Programming as I could fit into 5 days\n");
+    //printf("Ted has done as much iOS Programming as I could fit into 5 days\n");
+    
+    congratulateStudent("Mark", "Cocoa", 5);
+    congratulateStudent("Bo", "Objective-C", 2);
+    congratulateStudent("Ted", "iOS", 5);
     
     return 0;
 }
