d725ae093c26d603e4a02781bcf275de28880adc
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-02-27 08:15:26 +0800

    first Foundation project (uses NSDate)

---
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -0,0 +1,41 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Nestor Pestelos on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *now = [NSDate date];
+        NSLog(@"The new date lives at %@", now);
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970.", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@", later);
+        
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear:1981];
+        [comps setMonth:2];
+        [comps setDay:12];
+        [comps setHour:13];
+        [comps setMinute:0];
+        [comps setSecond:0];
+        
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+        
+        double d = [now timeIntervalSinceDate:dateOfBirth];
+        NSLog(@"I'm alive for %f seconds\n", d);
+        
+    }
+    return 0;
+}
+
