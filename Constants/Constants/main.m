215a2ab692b6dae4c7028bfde90276d0542306b7
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-03-02 15:21:25 +0800

    Constants

---
+++ Constants/Constants/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by Nestor Pestelos on 3/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // In literal NSString, use \u for arbitrary unicode chars
+        NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10, 12));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
+    }
+    return 0;
+}
+
