0297ac4dfd023604a593965a71ea2351e197e017
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-03-02 19:37:12 +0800

    helpers

--- Callbacks/Callbacks/Logger.m
+++ Callbacks/Callbacks/Logger.m
@@ -15,4 +15,35 @@
     NSLog(@"Ouch!");
 }
 
+// Called each time a chunk of data arrives
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    NSLog(@"received %lu bytes", [data length]);
+
+    // Create a mutable data if it doesn't already exist
+    if (!incomingData) {
+        incomingData = [[NSMutableData alloc] init];
+    }
+    
+    [incomingData appendData:data];
+}
+
+// Called when the last chunk has been processed
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"Got it all!");
+    NSString *string = [[NSString alloc] initWithData:incomingData encoding:NSUTF8StringEncoding];
+    incomingData = nil;
+    NSLog(@"string has %lu characters", [string length]);
+    
+    NSLog(@"The whole string is %@", string);
+}
+
+// Called if the fetch fails
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"Connection failed: %@", [error localizedDescription]);
+    incomingData = nil;
+}
+
 @end
